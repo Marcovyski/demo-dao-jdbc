d66c1e3436460afb8ccc42244f0ea3057ea3df37
Marcovyski <marco_a_costa@hotmail.com>
2021-11-25 19:33:23 -0300

    findById implementation

--- src/application/Program244.java
+++ src/application/Program244.java
@@ -11,13 +11,19 @@ import model.entities.Seller;
 public class Program244 {
 
 	public static void main(String[] args) {
-
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		/*
 		Department obj = new Department(1, "books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		
 		System.out.println(obj);
+		*/
 		System.out.println(seller);
 	}
 }
